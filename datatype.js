6551e872507958849cea0627b13fe33f4276e76f
Raju_0005 <rajugbp2006@gmail.com>
2025-11-23 05:55:13 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,32 @@
+// Types of DataTypes:(7 Types)
+
+// Primitive Data Types:
+// 1. string
+let name = "Raju Sen";
+
+// 2.Number // BigInt(use for larger Number)
+let age = "19";
+
+// 3.boolean
+let isLoggedIn = true; //(true/false)
+
+// 4.null (standalone value)
+let state = null; //[empty]
+
+// 5.undefined
+let state2;
+
+// 6.symbol (use for uniqueness)
+
+// Complex Data Types:
+// 7.Object (A collection of key-value pairs)
+let person = {
+  firstName: "Jane",
+  lastName: "Smith",
+  age: 25,
+};
+
+let colors = ["red", "green", "blue"]; // Array is an object
+
+// this is use find a type of variable datatypes
+console.log(typeof age);
--- variables.js
+++ variables.js
@@ -1,13 +1,13 @@
-const accountId = 1234567890 //const ko redeclare nhi kr skte
-let accountHolder = "Raju sen"
-var holderMobile = "9878098798"
-holderAddress = "Bhilwara"
-let accountType  // This value is Undefined because value is not defined
+const accountId = 1234567890; //const ko redeclare nhi kr skte
+let accountHolder = "Raju sen";
+var holderMobile = "9878098798";
+holderAddress = "Bhilwara";
+let accountType; // This value is Undefined because value is not defined
 
 // let varibles ko redeclare krne ke liye ese krna pdta h agr iske alawa sidhe declare kroge toh error dega
 {
-    let accountHolder = "Ram ji"
-    console.log(accountHolder);
+  let accountHolder = "Ram ji";
+  console.log(accountHolder);
 }
 
 // var holderMobile = "0989098909" //var is redeclare and allowed
@@ -18,15 +18,19 @@ let accountType  // This value is Undefined because value is not defined
 
 // console.log(holderAddress);
 
-// is console ko use krne se value table mein fetch hogi 
-console.table([accountId, accountHolder, holderMobile, holderAddress, accountType])
-
+// is console ko use krne se value table mein fetch hogi
+console.table([
+  accountId,
+  accountHolder,
+  holderMobile,
+  holderAddress,
+  accountType,
+]);
 
 // Note:
 // 1. const and let ko redeclare nhi kr skte
 // 2. let varibles ko redeclare krne ke liye {} use krna hota h
 // 3. var ko redeclare kr skte h (ab ise use nhi kiya jata and ise ab use nhi krna)
-
 
 // variables = const,let and var
 // key = accountId, accountHolder, holderMobile
