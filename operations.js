23213e46c6edb5c8181cb692bba36739acf9aa3e
Raju_0005 <rajugbp2006@gmail.com>
2025-11-24 12:05:24 +0000

    operations

---
+++ operations.js
@@ -0,0 +1,37 @@
+let score = 5
+let negscore = -3
+// console.log(negscore);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%3)
+
+
+let str1 = "Hello"
+let str2 = "Raju"
+let str3 = str1 + " " + str2
+// console.log(str3)
+
+
+// starting me string h toh baki sb ko bhi string me dekha jayega and agar number h toh number mein
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(3 + 4 * 5 % 6);//not readable
+// console.log((3 + 4) * 5 % 6);//redable
+
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
