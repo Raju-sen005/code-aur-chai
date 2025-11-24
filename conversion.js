7f98c6706ad7f1fac48395e7adce538dd9080cf5
Raju_0005 <rajugbp2006@gmail.com>
2025-11-24 05:09:47 +0000

    conversion of datatypes

---
+++ conversion.js
@@ -0,0 +1,23 @@
+let score = "55abc"
+// console.log(typeof score);
+
+let stringToNumber = Number(score)
+// console.log(typeof stringToNumber);
+// console.log(stringToNumber);
+
+
+// "33"=> 33
+// "33abc"=> NaN
+// null => 0
+// undifined => NaN
+//True => 1 ; False => 0
+
+
+let isLogin = 0
+let convertBoolean = Boolean(isLogin)
+console.log(convertBoolean);
+
+// 1=> true
+// 0=> false
+// ""=>false
+// "Raju"=>true
