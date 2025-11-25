3740a1fa427d13d1e356ff3bed758a816797fd5f
Raju_0005 <rajugbp2006@gmail.com>
2025-11-25 07:29:42 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,22 @@
+// Best Comparison:
+//console.log(2 > 1) // 2, 1 se bda h
+//console.log(2 >= 1) // 2, 1 se bda ya uske brabar h
+
+//console.log(2 < 1); // 2, 1 se chota h
+//console.log(2 <= 1); // 2, 1 se chota ya uske brabar h
+
+//console.log(2 == 1); // 2, 1 ke brabar h
+//console.log(2 != 1); // 2, 1 ke brabar nhi h
+
+
+// iska output toh shi aayega pr ye comparison shi nhi h kyuki comparison mein dono value ka database same hona chahiye:
+console.log("2" > 1)
+console.log("2" < 1)
+
+
+// not recommended Comparison:
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
