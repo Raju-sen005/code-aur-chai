cda3ea75f6ec282326ba40d364a674ef0f40b14e
Raju_0005 <rajugbp2006@gmail.com>
2025-11-26 09:56:26 +0000

    datatype_summary

---
+++ datatype_summary.js
@@ -0,0 +1,36 @@
+// Primitive Type
+// 7 type : String, Number, Boolean, Null, Undifined, Bigint, Symbol
+
+// const score = 100
+// const score2 = 100.3
+
+// const isLoggedIn = true
+
+// const outsideTemp = null
+
+// const id = Symbol('123')
+// const anotherId = Symbol("123")
+
+// console.log(id === anotherId)
+
+// console.log(typeof outsideTemp);
+
+// const bigNumber = 1234567890000000000n
+// console.log(typeof bigNumber);
+
+
+// Refrence Type (non-Primitive)
+// Objects, Array, Function
+const arr = ["a", "b", "c", "d"]
+let obj = {
+    "name": "Raju",
+    "age": 19
+}
+console.log(arr);
+console.log(obj);
+
+const myfunction = function () {
+    console.log("Hello");
+
+}
+
