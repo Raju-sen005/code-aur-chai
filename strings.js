6cb9e14cc7db603969619dbbb4f6cb6e6a4f3ffa
Raju_0005 <rajugbp2006@gmail.com>
2025-11-30 05:28:40 +0000

    Strings

--- strings.js
+++ strings.js
@@ -1,4 +1,14 @@
 const name = "Raju"
 const repoCount = 50
-console.log(name + repoCount); //This type is normal 
-console.log(`Hello my name is ${name} and my git repo count is ${repoCount}`);
+// console.log(name + repoCount); //This type is normal 
+// console.log(`Hello my name is ${name} and my git repo count is ${repoCount}`);
+
+
+const name2 = new String('Raju sen');
+
+console.log(name2[0]);
+console.log(name2.__proto__);
+console.log(name2.length);// This is use by find string length
+
+console.log(name2.charAt(3));// iska use kisi index pr konsa character h woh dekhne ke liye 
+console.log(name2.indexOf('a'));// iska use kisi character pr konsi index h woh dekhne ke liye
