e8b654c7afcf2efe79d5c471a1f59c18a9cd2aa1
Raju_0005 <rajugbp2006@gmail.com>
2025-12-02 06:06:27 +0000

    complete string

--- strings.js
+++ strings.js
@@ -4,11 +4,34 @@ const repoCount = 50
 // console.log(`Hello my name is ${name} and my git repo count is ${repoCount}`);
 
 
+// String Method:
+
 const name2 = new String('Raju sen');
 
-console.log(name2[0]);
-console.log(name2.__proto__);
-console.log(name2.length);// This is use by find string length
+//console.log(name2.__proto__);
+//console.log(name2[0]);
+//console.log(name2.length);// This is use by find string length
 
-console.log(name2.charAt(3));// iska use kisi index pr konsa character h woh dekhne ke liye 
-console.log(name2.indexOf('a'));// iska use kisi character pr konsi index h woh dekhne ke liye
+//console.log(name2.charAt(3));// iska use kisi index pr konsa character h woh dekhne ke liye 
+
+//console.log(name2.indexOf('a'));// iska use kisi character pr konsi index h woh dekhne ke liye
+
+// const newName2 = name2.substring(1, 4)
+// console.log(newName2);
+
+// const anotherName2 = name2.slice(2, 4)//isme nagetive value bhi de skte ho
+// console.log(anotherName2);
+
+// const newString1 = "   Raju  "
+// console.log(newString1);
+// console.log(newString1.trim());//space ko htane ke liye
+
+// const url = "Https://my%20com"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('raju'));//key me value h ya nhi ye check krne ke liye
+
+const convertArr = "raju is my name"
+console.log(convertArr.split(" "));//string ko array me convert krne ke liye
+
+
+// string link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
