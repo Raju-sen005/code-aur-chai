22c3b2b067b5ea175ee417ac40cb09eae2911659
Raju_0005 <rajugbp2006@gmail.com>
2025-12-15 06:42:24 +0000

    array_functions

---
+++ arrayfunction.js
@@ -0,0 +1,22 @@
+const marvel_hero = ["thor", "spiderman", "ironman"]
+const dc_hero = ["superman", "batman", "flash"]
+
+// marvel_hero.push(dc_hero) 
+// marvel_hero.concat(dc_hero)
+// const new_heros = [...marvel_hero, ...dc_hero]//jyada use
+// console.log(new_heros);
+
+// console.log(marvel_hero);
+// console.log(marvel_hero[3][1]);
+
+const another_array = [1,2,3,[4,5],7,[6,7,8,[9,0]]]
+// console.log(another_array.flat(Infinity));
+
+// console.log(Array.isArray("Raju"));
+// console.log(Array.from("Raju"));
+// console.log(Array.from({name:"raju"}));//interresting
+
+const Score1=100
+const Score2 =200
+const Score3=300
+console.log(Array.of(Score1,Score2,Score3))//multiple array ko ek mein convert krne ke liye
